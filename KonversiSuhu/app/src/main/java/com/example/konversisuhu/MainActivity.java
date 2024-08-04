1dccbc2a5d9f3cdfbf2b46f7953ab0e7cf1e1956
NaufalAltofianoKholis <140329480+NaufalAltofianoKholis@users.noreply.github.com>
2024-08-04 07:09:24 +0700

    vid-16

--- KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
+++ KonversiSuhu/app/src/main/java/com/example/konversisuhu/MainActivity.java
@@ -1,6 +1,12 @@
 package com.example.konversisuhu;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +15,92 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
+    EditText etNilai;
+    TextView tvHasil;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        load();
+//        isiSpinner();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public  void load(){
+        spinner = findViewById(R.id.spinner);
+        etNilai=findViewById(R.id.etNilai);
+        tvHasil=findViewById(R.id.tvHasil);
+    }
+
+//    public void isiSpinner(){
+//        String[] isi ={"Celcius to Reamur","Celcius to Farenheit","Celcius to Kelvin",};
+//        ArrayAdapter<String>adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,isi);
+//        spinner.setAdapter(adapter);
+//    }
+
+    public void btnKonversi(View view) {
+        String pilihan = spinner.getSelectedItem().toString();
+        if (etNilai.getText().toString().equals("")) {
+            Toast.makeText(this, "Nilai tidak boleh kosong", Toast.LENGTH_SHORT).show();
+        } else {
+            if (pilihan.equals("Celcius to Reamur")) {
+                cTor();
+            }
+            else if (pilihan.equals("Celcius to Farenheit")) {
+                cTof();
+            }
+            else if (pilihan.equals("Celcius to Kelvin")) {
+                cTok();
+            }
+            else if (pilihan.equals("Farenheit to Celcius")) {
+                fToc();
+            }
+            else if (pilihan.equals("Farenheit to Farenheit")) {
+                fTor();
+            }
+            else if (pilihan.equals("Farenheit to Kelvin")) {
+                fTok();
+            }
+        }
+    }
+    public void cTor(){
+        double suhu =Double.parseDouble(etNilai.getText().toString());
+        double hasil =((double) 4/5)*suhu;
+        tvHasil.setText(hasil+"");
+    }
+    public void cTof(){
+        double suhu =Double.parseDouble(etNilai.getText().toString());
+        double hasil =((double) 9/5)*suhu+32;
+        tvHasil.setText(hasil+"");
+    }
+    public void cTok(){
+        double suhu =Double.parseDouble(etNilai.getText().toString());
+        double hasil =suhu+273;
+        tvHasil.setText(hasil+"");
+    }
+    public void fToc(){
+        double suhu =Double.parseDouble(etNilai.getText().toString());
+        double hasil =((double) 5/9)*(suhu-32);
+        tvHasil.setText(hasil+"");
+    }
+    public void fTor(){
+        double suhu =Double.parseDouble(etNilai.getText().toString());
+        double hasil =((double) 4-9)*(suhu-32);
+        tvHasil.setText(hasil+"");
+    }
+    public void fTok(){
+        double suhu =Double.parseDouble(etNilai.getText().toString());
+        double hasil =((double)5/9)*(suhu-32)+273;
+        tvHasil.setText(hasil+"");
+    }
+
+
 }
---
+++ MessageDialog/app/src/main/java/com/example/messagedialog/MainActivity.java
@@ -0,0 +1,74 @@
+package com.example.messagedialog;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void showToast(String pesan){
+        Toast.makeText(this, pesan, Toast.LENGTH_SHORT).show();
+    }
+
+    public void showAlert(String pesan){
+        AlertDialog.Builder buatAlert =new AlertDialog.Builder(this);
+        buatAlert.setTitle("PERHATIAN !");
+        buatAlert.setMessage(pesan);
+
+        buatAlert.show();
+    }
+
+    public void showAlertButton(String pesan){
+        AlertDialog.Builder showAlert =new AlertDialog.Builder(this);
+        showAlert.setTitle("PERINGATAN !");
+        showAlert.setMessage(pesan);
+
+
+        showAlert.setPositiveButton("YA", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                showToast("Data sudah dihapus");
+            }
+        });
+        showAlert.setNegativeButton("Tidak", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                showToast("Data tidak dihapus");
+            }
+        });
+
+        showAlert.show();
+    }
+
+    public void btnToast(View view) {
+        showToast("belajar");
+    }
+
+    public void btnAlert(View view) {
+        showAlert("Silahkan Dicoba");
+    }
+
+    public void btnAlertDialogButton(View view) {
+        showAlertButton("yakin akan dihapus ?");
+    }
+}
